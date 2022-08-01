b89eed4b486c2cd68ce3e89f72eeaf30d2a8b5cb
oskip123 <56744714+oskip123@users.noreply.github.com>
2022-07-31 22:46:27 -0400

    syes

--- js/hero-slider-main.js
+++ js/hero-slider-main.js
@@ -139,3 +139,20 @@ jQuery(document).ready(function($){
 	    return this;
 	};
 });
+
+function myFunction() {
+	document.getElementById("myDropdown").classList.toggle("show");
+  }
+  
+  window.onclick = function(event) {
+	if (!event.target.matches('.dropbtn')) {
+	  var dropdowns = document.getElementsByClassName("dropdown-content");
+	  var i;
+	  for (i = 0; i < dropdowns.length; i++) {
+		var openDropdown = dropdowns[i];
+		if (openDropdown.classList.contains('show')) {
+		  openDropdown.classList.remove('show');
+		}
+	  }
+	}
+  }
